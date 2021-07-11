e5b14539e093b690a43f7d55628796659a041b85
vaidicbooks <vaidicbooks@gmail.com>
2021-07-11 11:18:04 +0530

    Removed previous url
    r

--- upnishad/isha/java.js
+++ upnishad/isha/java.js
@@ -1,8 +1,8 @@
 function fire() {
   var page = document.getElementById('select').value;
     if (page == "") {
-        window.location = "http://vedicbooks.cf/upnishad/isha"      
+        window.location = "http://"+url+"/upnishad/isha"      
      } else {
-         window.location= "http://"+url+"/upnishad/isha/" +page;     
+         window.location= "http://"+url+"/upnishad/isha/" +page;
         }
 }
